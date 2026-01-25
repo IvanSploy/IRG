954b200d451b4e1c5887f5e819596d451b2df4a8
Iván Rodríguez García <ivansploy672@gmail.com>
2026-01-25 19:47:58 +0100

    UPD - Improved Factories working.
    +In order to avoid problems using multiple assemblies, it has been improved for detecting all the types in all the assemblies.

--- Runtime/Utils/GenericFactory.cs
+++ Runtime/Utils/GenericFactory.cs
@@ -7,15 +7,19 @@ namespace IRG
 {
     public static class GenericFactory<TGeneric> where TGeneric : class
     {
-        private static bool _isInit;
         private static readonly Dictionary<Type, Type> _map = new();
 
         private static void Initialize()
         {
             _map.Clear();
-            var types = Assembly.GetAssembly(typeof(TGeneric))
-                .GetTypes()
-                .Where(t => typeof(TGeneric).IsAssignableFrom(t) && !t.IsAbstract);
+            var types =
+#if UNITY_EDITOR
+                UnityEditor.TypeCache.GetTypesDerivedFrom<TGeneric>()
+                    .Where(t => !t.IsAbstract);
+#else
+                AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly =>
+                    assembly.GetTypes().Where(t => typeof(TGeneric).IsAssignableFrom(t) && !t.IsAbstract));
+#endif
 
             foreach (var type in types)
             {
@@ -29,32 +33,35 @@ namespace IRG
                 var attr = baseType.GetGenericArguments();
                 if (attr.Length > 0) _map[attr[0]] = type;
             }
-
-            _isInit = true;
         }
         
         public static Type GetKey(Type type)
         {
-            if (!_isInit) Initialize();
             foreach (var kvp in _map)
             {
                 if (kvp.Value == type)
                     return kvp.Key;
             }
 
-            _isInit = false;
+            Initialize();
+            
+            foreach (var kvp in _map)
+            {
+                if (kvp.Value == type)
+                    return kvp.Key;
+            }
+
             throw new InvalidOperationException($"Type {type} is not registered.");
         }
 
         public static TGeneric Create(Type type)
         {
-            if(!_isInit) Initialize();
+            if(!_map.ContainsKey(type)) Initialize();
             if (_map.TryGetValue(type, out var graphNodeType))
             {
                 return (TGeneric)Activator.CreateInstance(graphNodeType);
             }
 
-            _isInit = false;
             throw new InvalidOperationException($"No element registered for data type {type.Name}");
         }
     }
